2962d4be5e5a9195f83914f8630fbf473fd8f2eb
Yasser Moradi <ysmoradi@outlook.com>
2016-10-20 17:30:17 +0330

    Added owin app test using TestServer

---
+++ OwinKatanaSample.Tests/Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OwinKatanaSample.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public async Task TestWebApi()
+        {
+            using (TestServer server = TestServer.Create<OwinAppStartup>())
+            {
+                HttpResponseMessage response = await server.HttpClient.GetAsync("/api/products/1");
+                response.EnsureSuccessStatusCode();
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+    }
+}
