ac8f9637e1f63103dee8d7e6fd2d3e29d1d66b55
Yasser Moradi <ysmoradi@outlook.com>
2016-10-20 17:30:02 +0330

    Added web api, web api odata & signalr configs

---
+++ OwinKatanaSample/ApiControllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using OwinKatanaSample.Model;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace OwinKatanaSample.ApiControllers
+{
+    public class ProductsController : ApiController
+    {
+        [HttpGet]
+        [Route("products/{categoryId}")]
+        public List<Product> GetProductsByCategoryId(int categoryId)
+        {
+            return new List<Product>
+            {
+                new Product { Id = 1 , Name = "Test" }
+            };
+        }
+    }
+}
---
+++ OwinKatanaSample/ODataControllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using OwinKatanaSample.Model;
+using System.Web.Http;
+using System.Web.OData;
+
+namespace OwinKatanaSample.ODataControllers
+{
+    public class CategoriesController : ODataController
+    {
+        [HttpGet]
+        public Category GetBestCategory()
+        {
+            return new Category { Id = 1, Name = "Test" };
+        }
+    }
+}
